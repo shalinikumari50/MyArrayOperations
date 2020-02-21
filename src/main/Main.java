7d93bcb0e97f788d752965fca913fb34639e521e
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-02-21 17:54:42 +0530

    take input from user and perform specified operation

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,5 +9,72 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         ArrayOperations arrayOperations = new ArrayOperations();
         arrayOperations.printMenu();
+        int choice = -1;
+        do {
+            System.out.println("Enter choice : ");
+            try {
+                choice = Integer.parseInt(scanner.nextLine().trim());
+            } catch (NumberFormatException nfe) {
+                System.out.println("Invalid input");
+                continue;
+            }
+            switch (choice) {
+                case 0:
+                    break;
+                case 1:
+                    try {
+                        arrayOperations.createArray();
+                    } catch (NumberFormatException nfe) {
+                        System.out.println("Invalid input");
+                    } catch (NegativeArraySizeException nae) {
+                        System.out.println("Array size can not be negative");
+                    }
+                    break;
+                case 2:
+                    arrayOperations.display();
+                    break;
+                case 3:
+                    try {
+                        arrayOperations.insert();
+                    } catch (NumberFormatException nfe) {
+                        System.out.println("Invalid input");
+                    }
+                    break;
+                case 4:
+                    try {
+                        arrayOperations.delete();
+                    } catch (NumberFormatException nfe) {
+                        System.out.println("Invalid input");
+                    }
+                    break;
+                case 5:
+                    try {
+                        arrayOperations.deleteIndex();
+                    } catch (NumberFormatException nfe) {
+                        System.out.println("Invalid input");
+                    }
+                    break;
+                case 6:
+                    try {
+                        arrayOperations.search();
+                    } catch (NumberFormatException nfe) {
+                        System.out.println("Invalid input");
+                    }
+                    break;
+                case 7:
+                    arrayOperations.sortArrayInAscendingOrder();
+                    break;
+                case 8:
+                    arrayOperations.printMenu();
+
+                    break;
+                default:
+                    System.out.println("Please enter valid choice");
+
+
+            }
+
+
+        } while (choice != 0);
     }
 }
