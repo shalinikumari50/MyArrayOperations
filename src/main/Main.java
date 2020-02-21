0bbcc8d866a750731891da8bbd3f3c780eee9fed
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-02-21 17:48:15 +0530

    add object : arrayOperations of class : ArrayOperations

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,12 @@
 package main;
 
+import arrayOperations.ArrayOperations;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayOperations arrayOperations = new ArrayOperations();
     }
 }
