b09efdf09a710a9740976b037cb75a862328fcf6
shalinikumari50 <shalini.kumari_cs18@gla.ac.in>
2020-02-21 16:38:32 +0530

    implement interface : Operations

--- src/arrayOperations/ArrayOperations.java
+++ src/arrayOperations/ArrayOperations.java
@@ -1,4 +1,25 @@
 package arrayOperations;
 
-public class ArrayOperations {
+import operations.Operations;
+
+public class ArrayOperations implements Operations {
+    @Override
+    public int display() {
+        return 0;
+    }
+
+    @Override
+    public int search() {
+        return 0;
+    }
+
+    @Override
+    public int insert() {
+        return 0;
+    }
+
+    @Override
+    public int delete() {
+        return 0;
+    }
 }
